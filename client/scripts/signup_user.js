3112d72717df9bea1aad301f158a29dcd2261480
ivamacio <ivan4341@gmail.com>
2014-11-23 03:12:49 -0500

    Updated endpoints

--- client/scripts/signup_user.js
+++ client/scripts/signup_user.js
@@ -39,10 +39,21 @@
 					.then(function (me) {
 						$scope.loggedUser = me;
 
+						$http.get('/api/user/'+me.id).
+						  success(function(data, status, headers, config) {
+						  	console.log("Success2", data)
+						    // this callback will be called asynchronously
+						    // when the response is available
+						  }).
+						  error(function(data, status, headers, config) {
+						    // called asynchronously if an error occurs
+						    // or server returns response with an error status.
+						    console.log("2Error when was trying to save the logged user.");
+						  });
+
 						$http.post(ENDPOINT, me)
 					        .success(function (response) {
 					          console.log("Success")
-					          console.log(me)
 					        })
 					        .error(function (response) {
 					          console.log("Error when was trying to save the logged user.");
--- server/api/users/index.js
+++ server/api/users/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = module.exports = express();
 var User = require('./user.model');
+var Class = require('../classes/class.model');
 
 app.post('/api/signup/user', function (req, res) {
 	req.body.facebook_user_id = req.body.id;
@@ -18,4 +19,47 @@ app.post('/api/signup/user', function (req, res) {
 
 	res.sendStatus(200);
 	});
+});
+
+app.get('/api/user/:id', function (req, res) {
+	var id = req.param('id');
+
+	User.findOne({'facebook_user_id': req.params.id}).exec(function (err, user) {
+		if (err) {
+			res.sendStatus(500);
+			return;
+		}
+
+		res.status(200).json(user);
+	});
+});
+
+app.put('/api/user/:userId/:classId', function(req, res) {
+	var userId = req.param('userId');
+	var classId = req.param('classId');
+
+	User.findOne({'facebook_user_id': userId})
+		.exec(function (err, user) {
+			if (err) {
+				res.status(400).send('No user with id', userId);
+				return;
+			}
+
+			if (user.class_ids.indexOf(classId) !== -1) {
+				res.status(400).send('User ' + userId + ' already has class with id ' + classId);
+				return;
+			}
+
+			user.class_ids.push(classId);
+			user.save(onNewUserClass);
+		});
+
+	function onNewUserClass(err, result) {
+		if (error) {
+			res.sendStatus(500);
+			return;
+		}
+
+		res.sendStatus(200);
+	}
 });
