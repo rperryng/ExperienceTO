5552108e59290f85fdb5f418887df68b4453f80d
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2014-11-23 03:01:09 -0500

    Fixed merge.  Fixed script ordering.  Refactored http calls into service

--- client/scripts/signup_user.js
+++ client/scripts/signup_user.js
@@ -1,57 +1,86 @@
 (function () {
-	'use strict';
+  'use strict';
 
-	angular.module('ExploreTO', ['facebook']);
+  angular.module('ExploreTO', ['facebook']);
 })();
 
 (function () {
-	'use strict';
+  'use strict';
 
-	angular
-		.module('ExploreTO')
-		.controller('MainController', MainController);
+  angular
+    .module('ExploreTO')
+    .controller('MainController', MainController);
 
-	MainController.$inject = ['$scope', '$http', 'facebook'];
+  MainController.$inject = ['$scope', '$http', 'facebook', 'userSignupFactory'];
 
-	function MainController($scope, $http, facebook) {
-		var ENDPOINT = '/api/signup/user';
-		var vm = this;
+  function MainController($scope, $http, facebook, userSignupFactory) {
+    var vm = this;
 
-		console.log('listening');
-		var stopListening = $scope.$on('facebookReady', onFacebookReady);
+    console.log('listening');
+    var stopListening = $scope.$on('facebookReady', onFacebookReady);
 
-		//////////
+    //////////
 
-		function onFacebookReady() {
-			console.log('done...');
-			stopListening();
+    function onFacebookReady() {
+      console.log('done...');
+      stopListening();
 
-			facebook.login().then(function (connected) {
-				if (connected) {
-					getMe();
-				} else {
-					console.log("Error when was trying to log in on facebook.");
-				}
-			});
+      facebook.login().then(function (connected) {
+        if (connected) {
+          getMe();
+        } else {
+          console.log("Error when was trying to log in on facebook.");
+        }
+      });
 
-			function getMe() {
-				facebook.getMe()
-					.then(function (me) {
-						$scope.loggedUser = me;
+      function getMe() {
+        facebook.getMe()
+          .then(function (me) {
+            vm.facebookUserId = me.id;
+            vm.firstName = me.first_name;
 
-						$http.post(ENDPOINT, me)
-					        .success(function (response) {
-					          console.log("Success")
-					          console.log(me)
-					        })
-					        .error(function (response) {
-					          console.log("Error when was trying to save the logged user.");
-					        });
+            userSignupFactory.registerUser(me)
+              .then(function () {
+                console.log('success');
+              });
+          });
+      }
+    }
+  }
+})();
 
-					}, function (error) {
+(function () {
+  'use strict';
 
-					});
-			}
-		}
-	}
+  angular
+    .module('ExploreTO')
+    .factory('userSignupFactory', userSignupFactory);
+
+  userSignupFactory.$inject = ['$q', '$http'];
+
+  function userSignupFactory($q, $http) {
+    var factory = {
+      registerUser: registerUser
+    };
+
+    return factory;
+
+    //////////
+
+    function registerUser(me) {
+      var deferred = $q.defer();
+      var url = '/api/signup/user';
+
+      $http.post(url, me)
+        .success(function () {
+          deferred.resolve();
+        })
+        .error(function () {
+          deferred.reject();
+        });
+
+      return deferred.promise;
+    }
+  }
+
 })();
